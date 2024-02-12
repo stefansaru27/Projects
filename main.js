1ebeea3666ccad41254265670b9ed5616b1d3d76
stefansaru27 <58091554+stefansaru27@users.noreply.github.com>
2024-02-12 22:09:41 +0200

    Homework-Week13-Session-2-SpreadOperator

--- main.js
+++ main.js
@@ -1,108 +1,55 @@
-class Book {
-  constructor(title, author, isbn) {
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-  }
-
-  displayInfo() {
-    console.log(
-      `Book title: ${this.title} -- Author: ${this.author}  -- ISBN: ${this.isbn}`
-    );
-  }
+//Write a function that can take in any number of arguments, and returns the sum of all of the arguments.
+function sum(...numbers) {
+  return numbers.reduce((acc, num) => acc + num, 0);
 }
 
-class FictionBook extends Book {
-  constructor(title, author, isbn, additionalGenre) {
-    super(title, author, isbn);
-    this.additionalGenre = additionalGenre;
-  }
+console.log(sum(5, 10, 15));
 
-  displayInfo() {
-    console.log(
-      `Fiction Book title: ${this.title} -- Author: ${this.author} -- Additional Genre: ${this.additionalGenre} -- ISBN: ${this.isbn}`
-    );
-  }
-}
-class Library {
-  constructor(books) {
-    this.books = books;
-  }
-  addBook(newBook) {
-    this.books.push(newBook);
-    console.log(
-      `In this library you have added the following books: ${newBook.title}`
-    );
-  }
-  displayLibrary() {
-    console.log("Your library has the following books: ");
-    this.books.forEach(function (book) {
-      console.log(book.title);
-    });
-  }
+//Write a function that can take in any number of arguments (including numbers or strings), and returns the sum of only the numbers.
+function sumNumbers(...arguments) {
+  return arguments
+    .filter((arguments) => typeof arguments === "number" && !isNaN(arguments))
+    .reduce((acc, num) => acc + num, 0);
 }
 
-class LibraryMember {
-  constructor(name, library) {
-    this.name = name;
-    this.library = library;
-    this.booksBorrowed = [];
-  }
-  borrowBook(book) {
-    // Check if the book is available in the library
-    if (this.library.books.includes(book)) {
-      // Add the book to the member's borrowed books
-      this.booksBorrowed.push(book);
-      console.log(`${this.name} has borrowed the book: ${book.title}`);
-    } else {
-      console.log(
-        `${this.name}, the book ${book.title} is not available in the library.`
-      );
-    }
-  }
-  displayBorrowedBooks() {
-    console.log(`${this.name}'s borrowed books:`);
-    this.booksBorrowed.forEach(function (book) {
-      console.log(book.title);
-    });
-  }
+console.log(sumNumbers(2, "4", 6, 8, "10"));
+
+//Write a function that takes in two arrays as arguments, and returns a single array that combines both (using the spread operator).
+function concatArrays(array1, array2) {
+  return [...array1, ...array2];
+}
+const array1 = [1, 3, 7];
+const array2 = [2, 4, 10];
+
+const concatArray = concatArrays(array1, array2);
+console.log(concatArray);
+
+//Write a function that takes in any amount of arguments, and returns the sum of every other argument
+function sumEveryOther(...args) {
+  return args
+    .filter((_, index) => index % 2 === 0)
+    .reduce((sum, num) => sum + num, 0);
 }
 
-//tests
-var book1 = new Book("Rich dad, poor dad", "Robert Kiosaki", 12345);
-var book2 = new Book("Emotional Inteligence", "Daniel Goleman", 12346);
+const result = sumEveryOther(1, 2, 3, 4, 5, 6);
+console.log(result); // Output: 1 + 3 + 5 = 9
 
-var fictionBook1 = new FictionBook(
-  "Angels and Demons",
-  "Dan Brown",
-  12121,
-  "Drama"
-);
-var fictionBook2 = new FictionBook(
-  "The Da Vinci Code",
-  "Dan Brown",
-  12134,
-  "Thriller"
-);
+//Write a function that can take in any number of arguments, and returns an array of only the unique arguments.
+function uniqueArguments(...args) {
+  return [...new Set(args)];
+}
 
-var myBooksArray = [book1, fictionBook1];
+const uniqueArray = uniqueArguments(1, 2, 3, 2, 4, 5, 1, 6);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
 
-var library1 = new Library(myBooksArray);
-library1.addBook(book2);
-library1.addBook(fictionBook2);
+//Write a function that takes in any number of arrays as arguments and combines all of them into one array.
+function combineArrays(...arrays) {
+  return [].concat(...arrays);
+}
 
-var radu = new LibraryMember("Radu", library1);
+const arrayNum = [1, 2, 3];
+const arrayStr = ["a", "b", "c"];
+const arrayBool = [true, false];
 
-console.log(book1);
-console.log(fictionBook1);
-console.log(library1);
-
-book1.displayInfo();
-fictionBook1.displayInfo();
-library1.displayLibrary();
-
-console.log(radu);
-
-radu.borrowBook(book1);
-radu.borrowBook(fictionBook1);
-radu.displayBorrowedBooks();
+const combinedArray = combineArrays(arrayNum, arrayStr, arrayBool);
+console.log(combinedArray);
