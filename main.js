f10b44999229ff34164eaa0b572db9dadc1938aa
stefansaru27 <58091554+stefansaru27@users.noreply.github.com>
2024-02-12 22:05:51 +0200

    js-oop-es6

--- main.js
+++ main.js
@@ -1,70 +1,72 @@
-function Book(title, author, isbn) {
-  this.title = title;
-  this.author = author;
-  this.isbn = isbn;
-}
-//display function Book
-Book.prototype.displayInfo = function () {
-  console.log(
-    `Book title: ${this.title} -- Author: ${this.author}  -- ISBN: ${this.isbn}`
-  );
-};
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
 
-function FictionBook(title, author, isbn, additionalGenre) {
-  Book.call(this, title, author, isbn);
-  this.additionalGenre = additionalGenre;
-}
-FictionBook.prototype = Object.create(Book.prototype);
-FictionBook.prototype.constructor = FictionBook;
-//display function FictionBook overwrites the display function from Book class
-FictionBook.prototype.displayInfo = function () {
-  console.log(
-    `Fiction Book title: ${this.title} -- Author: ${this.author} -- Additional Genre: ${this.additionalGenre} -- ISBN: ${this.isbn}`
-  );
-};
-
-function Library(books) {
-  this.books = books;
-}
-//Function for adding books in the library
-Library.prototype.addBook = function (newBook) {
-  this.books.push(newBook);
-  console.log(
-    `In this library you have added the following books: ${newBook.title}`
-  );
-};
-Library.prototype.displayLibrary = function () {
-  console.log("Your library has the following books: ");
-  this.books.forEach(function (book) {
-    console.log(book.title);
-  });
-};
-
-function LibraryMember(name, library) {
-  this.name = name;
-  this.library = library;
-  this.booksBorrowed = [];
-}
-
-LibraryMember.prototype.borrowBook = function (book) {
-  // Check if the book is available in the library
-  if (this.library.books.includes(book)) {
-    // Add the book to the member's borrowed books
-    this.booksBorrowed.push(book);
-    console.log(`${this.name} has borrowed the book: ${book.title}`);
-  } else {
+  displayInfo() {
     console.log(
-      `${this.name}, the book ${book.title} is not available in the library.`
+      `Book title: ${this.title} -- Author: ${this.author}  -- ISBN: ${this.isbn}`
     );
   }
-};
+}
 
-LibraryMember.prototype.displayBorrowedBooks = function () {
-  console.log(`${this.name}'s borrowed books:`);
-  this.booksBorrowed.forEach(function (book) {
-    console.log(book.title);
-  });
-};
+class FictionBook extends Book {
+  constructor(title, author, isbn, additionalGenre) {
+    super(title, author, isbn);
+    this.additionalGenre = additionalGenre;
+  }
+
+  displayInfo() {
+    console.log(
+      `Fiction Book title: ${this.title} -- Author: ${this.author} -- Additional Genre: ${this.additionalGenre} -- ISBN: ${this.isbn}`
+    );
+  }
+}
+class Library {
+  constructor(books) {
+    this.books = books;
+  }
+  addBook(newBook) {
+    this.books.push(newBook);
+    console.log(
+      `In this library you have added the following books: ${newBook.title}`
+    );
+  }
+  displayLibrary() {
+    console.log("Your library has the following books: ");
+    this.books.forEach(function (book) {
+      console.log(book.title);
+    });
+  }
+}
+
+class LibraryMember {
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+    this.booksBorrowed = [];
+  }
+  borrowBook(book) {
+    // Check if the book is available in the library
+    if (this.library.books.includes(book)) {
+      // Add the book to the member's borrowed books
+      this.booksBorrowed.push(book);
+      console.log(`${this.name} has borrowed the book: ${book.title}`);
+    } else {
+      console.log(
+        `${this.name}, the book ${book.title} is not available in the library.`
+      );
+    }
+  }
+  displayBorrowedBooks() {
+    console.log(`${this.name}'s borrowed books:`);
+    this.booksBorrowed.forEach(function (book) {
+      console.log(book.title);
+    });
+  }
+}
 
 //tests
 var book1 = new Book("Rich dad, poor dad", "Robert Kiosaki", 12345);
