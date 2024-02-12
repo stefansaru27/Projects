10f33d4044770a727575405ce5832f9df51850f3
stefansaru27 <58091554+stefansaru27@users.noreply.github.com>
2024-02-12 22:03:31 +0200

    js-oop

--- main.js
+++ main.js
@@ -1,81 +1,59 @@
-const searchBtn = document.getElementById("search-btn");
-const mealList = document.getElementById("meal");
-const mealDetailsContent = document.querySelector(".meal-details-content");
-const recipeCloseBtn = document.getElementById("recipe-close-btn");
-
-//event listeners
-searchBtn.addEventListener("click", getMealList);
-mealList.addEventListener("click", getMealRecipe);
-recipeCloseBtn.addEventListener("click", () => {
-  mealDetailsContent.parentElement.classList.remove("showRecipe");
-});
-
-//get meal list that matches with the ingredients
-function getMealList() {
-  let searchInputTxt = document.getElementById("search-input").value.trim();
-  console.log(searchInputTxt.length);
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let html = "";
-      if (data.meals) {
-        data.meals.forEach((meal) => {
-          html += `
-          <div class="meal-item" data-id = "${meal.idMeal}">
-            <div class="meal-img">
-              <img src=${meal.strMealThumb} alt="food" />
-            </div>
-            <div class="meal-name">
-              <h3>${meal.strMeal}</h3>
-              <a href="#" class="recipe-btn">Get Recipe</a>
-            </div>
-          </div>
-          `;
-        });
-      } else {
-        html = "Sorry, we didn't find any meal!";
-        mealList.classList.add("notFound");
-      }
-      mealList.innerHTML = html;
-    });
+// Create a Highway with different types of vehicles (bus, car, motorcycle, etc..). Bus, Car Motorcycle instances inherit from Vehicle.
+function Vehicle(make, model) {
+  this.make = make;
+  this.model = model;
 }
 
-//get the recipe of the meal
-function getMealRecipe(e) {
-  e.preventDefault();
-  if (e.target.classList.contains("recipe-btn")) {
-    let mealItem = e.target.parentElement.parentElement;
-    fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        mealRecipeModal(data.meals);
-      });
+function Bus(make, model, capacity) {
+  Vehicle.call(this, make, model);
+  this.capacity = capacity;
+}
+Bus.prototype = Object.create(Vehicle.prototype);
+Bus.prototype.constructor = Bus;
+
+function Car(make, model, doorsNumber) {
+  Vehicle.call(this, make, model);
+  this.doorsNumber = doorsNumber;
+}
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car;
+
+function Motorcycle(make, model, hasSidecar) {
+  Vehicle.call(this, make, model);
+  this.hasSidecar = hasSidecar;
+}
+Motorcycle.prototype = Object.create(Vehicle.prototype);
+Motorcycle.prototype.constructor = Motorcycle;
+
+var bus1 = new Bus("Mercedes", "Citaro", 50);
+var car1 = new Car("Dacia", "Sandero", 5);
+var motorcycle1 = new Motorcycle("Harley-Davidson", "Sportster", true);
+
+console.log(bus1);
+console.log(car1);
+console.log(motorcycle1);
+
+// Create 2 Highways and allow a list of unique/different vehicles to run on it.
+function Highway(name) {
+  this.name = name;
+  this.vehicles = [];
+}
+
+Highway.prototype.addVehicle = function (vehicle) {
+  // Check if the vehicle is not already on the highway
+  if (!this.vehicles.includes(vehicle)) {
+    this.vehicles.push(vehicle);
+    console.log(`${vehicle.make} ${vehicle.model} is now on ${this.name}.`);
+  } else {
+    console.log(`${vehicle.make} ${vehicle.model} is already on ${this.name}`);
   }
-}
+};
 
-//create a modal
-function mealRecipeModal(meal) {
-  console.log(meal);
-  meal = meal[0];
-  let html = `              
-    <h2 class="recipe-title">${meal.strMeal}</h2>
-    <p class="recipe-category">${meal.strCategory}</p>
-    <div class="recipe-instruct">
-        <h3>Instructions:</h3>
-        <p>${meal.strInstructions}</p>
-    </div>
-    <div class="recipe-meal-img">
-        <img src="${meal.strMealThumb}" alt="" />
-    </div>
-    <div class="recipe-link">
-        <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
-    </div>
-    `;
+var highway1 = new Highway("Highway 1");
+var highway2 = new Highway("Highway 2");
 
-  mealDetailsContent.innerHTML = html;
-  mealDetailsContent.parentElement.classList.add("showRecipe");
-}
+// Add vehicle on the highway
+highway1.addVehicle(bus1);
+highway1.addVehicle(car1);
+
+highway2.addVehicle(motorcycle1);
